e7bdef2b75127c4f2a168b98b9d252e069dc3be2
VonRiddarn <vonriddarn@gmail.com>
2025-03-14 04:38:05 +0100

    feat; move super convoluted database solution to backend project instead.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import "./App.scss";
 
+const resb = await fetch("https://rickandmortyapi.com/api/character/302dsadsadsadsadsadsa");
+
+console.log(await resb.json());
+
 function App() {
 	return (
 		<>
